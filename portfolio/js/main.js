b9130c3094729856ad42b564496339673a749f13
Belosvet-star <63208646+Belosvet-star@users.noreply.github.com>
2020-08-21 18:33:45 +0300

    Add files via upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,12 @@
+new WOW().init();
+
+
+var mySwiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.project-pagination',
+    type: 'bullets',
+    bulletClass: 'project-bullet',
+    bulletActiveClass: 'project-bullet-active',
+    clickable: true,
+  },
+});
