2d719e4e8f0c0aaf9991b5aff2463812b4d41694
Belosvet-star <63208646+Belosvet-star@users.noreply.github.com>
2020-11-11 22:11:36 +0200

    Add files via upload

--- elka_portfolio/js/script.js
+++ elka_portfolio/js/script.js
@@ -338,12 +338,7 @@ function send(event, php){
   
   };
 
-  $(window).on("load", function () {
-    var $preloader = $("#page-preloader"),
-      $spinner = $preloader.find(".spinner");
-    $spinner.fadeOut();
-    $preloader.delay(500).fadeOut("slow");
-  });
+  
 
 
 
