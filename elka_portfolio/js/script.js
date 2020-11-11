290d53b440c8da654221a9168858d68f0e36f073
Belosvet-star <63208646+Belosvet-star@users.noreply.github.com>
2020-11-11 20:48:20 +0200

    Add files via upload

--- elka_portfolio/js/script.js
+++ elka_portfolio/js/script.js
@@ -268,7 +268,7 @@ jQuery(document).ready(function($){
 });
 
 
-$(document).ready(function(){
+/* $(document).ready(function(){
   $("#form-popup").submit(function(e) { //устанавливаем событие отправки для формы с id=form
     e.preventDefault();
     var form_data = $(this).serialize(); //собераем все данные из формы
@@ -278,14 +278,14 @@ $(document).ready(function(){
       data: form_data,
       success: function(data) {
             //код в этом блоке выполняется при успешной отправке сообщения
-            $('#text-popup').show('100');
+            $('.modal-thanks').show('100');
         },
         error: function(){
           alert('error')
         },
     });
   });
-});
+}); */
 
 
 
@@ -304,6 +304,39 @@ $(document).ready(function() {
     e.preventDefault();
     $('html, body').animate({scrollTop:0}, '300');
   });
+}); 
   
+
+
+
+
+  // Отправка данных на сервер
+function send(event, php){
+  console.log("Отправка запроса");
+  event.preventDefault ? event.preventDefault() : event.returnValue = false;
+  var req = new XMLHttpRequest();
+  req.open('POST', php, true);
+  req.onload = function() {
+    if (req.status >= 200 && req.status < 400) {
+    json = JSON.parse(this.response); // Ебанный internet explorer 11
+        console.log(json);
+
+        // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+        if (json.result == "success") {
+          // Если сообщение отправлено
+          alert("Сообщение отправлено");
+        } else {
+          // Если произошла ошибка
+          alert("Ошибка. Сообщение не отправлено");
+        }
+      // Если не удалось связаться с php файлом
+      } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
   
-});
+  // Если не удалось отправить запрос. Стоит блок на хостинге
+  req.onerror = function() {alert("Ошибка отправки запроса");};
+  req.send(new FormData(event.target));
+  
+  }
+
+
+
