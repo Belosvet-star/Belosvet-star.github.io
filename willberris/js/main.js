aa8821e61a45db89b34b32f4966b5ed94825e9f6
Belosvet-star <63208646+Belosvet-star@users.noreply.github.com>
2021-03-24 17:19:18 +0200

    Add files via upload

--- willberris/js/main.js
+++ willberris/js/main.js
@@ -24,11 +24,19 @@ const closeModal = function(){
 buttonCart.addEventListener('click', openModal);
 modalClose.addEventListener('click', closeModal);
 
+modalCart.addEventListener('click', function(event) {
+	const target = event.target;
+	if (target.classList.contains('overlay') || //contains - проверка на наличие класса
+		target.classList.contains('modal-close')) {
+		closeModal()
+	}
+});
+
 //плавная прокрутка scrill smooth? по с тандарту быстро проскакивает
 //получим все ссылки у кот класс scrolllink
 
 
-(function(){
+/* (function(){
 	const scrollLinks = document.querySelectorAll('a.scroll-link');
 
 	for (let i = 0; i < scrollLinks.length; i++) {
@@ -43,4 +51,109 @@ modalClose.addEventListener('click', closeModal);
 
 		});
 	}
+})() */
+
+//это то же, что и выше, но по другому
+(function(){
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (scrollLink of scrollLinks) {
+		scrollLink.addEventListener('click', function(event){
+			event.preventDefault();
+			const id = scrollLink.getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			})
+			console.log(id);
+
+		});
+	}
 })()
+
+//goods товары
+const more = document.querySelector('.more');
+const navigationLink = document.querySelectorAll('.navigation-link');
+const longGoodsList = document.querySelector('.long-goods-list');
+
+//функци, кот будет получать товары с сервера. У нас этол папка db и обычно это файл json
+
+const getGoods = async function() {
+	const result = await fetch('db/db.json'); //тут может быть урл адрес такого файла
+	//console.log(result);
+	if(!result.ok) {
+		throw 'ошибочка вышла:  ' + result.status
+	}
+	return await result.json();
+};
+
+/* getGoods().then(function(data){
+	console.log(data)
+}); */
+//console.log(getGoods());
+
+//выводить карточки.
+//вначале их создаем
+const createCard = function (objCard) {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6'
+
+
+	card.innerHTML = `
+		<div class="goods-card">
+			${objCard.label ?
+				'<span class="label">${objCard.label}</span>' :
+				''}
+			<img
+				src="db/${objCard.img}"
+				alt="${objCard.name}"
+				class="goods-image"
+			/>
+			<h3 class="goods-title">${objCard.name}</h3>
+			<p class="goods-description">${objCard.description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id="${objCard.id}">
+				<span class="button-price">$${objCard.price}</span>
+			</button>
+		</div>
+
+	`; //`` где ё это обратные кавычки - это шаблонныу строки, комментарии можно тут убрать
+
+	return card;
+};
+
+//показ этих карточек на странице
+const renderCards = function(data) {
+	longGoodsList.textContent = ''; 
+	const cards = data.map(createCard)
+	longGoodsList.append(...cards)
+	document.body.classList.add('show-goods')
+};
+
+more.addEventListener('click', function(event){
+	event.preventDefault();
+	getGoods().then(renderCards);
+});
+
+//фильтруем карточки по фенскому, мужсткому еще какому-то
+const filterCards = function(field, value){
+	getGoods()
+	.then(function (data) {
+		const filteredGoods = data.filter(function(good) {
+			return good[field] === value
+		});
+		return filteredGoods;
+	})
+	.then(renderCards);
+};
+
+navigationLink.forEach(function (link) {
+	link.addEventListener('click', function(event) {
+		event.preventDefault();
+		//console.log(link);
+		const field = link.dataset.field
+		const value = link.textContent;
+		//console.log(field, value);
+		filterCards(field, value)
+	})
+});
+
