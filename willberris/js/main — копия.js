50adf979b44b6ff65ae740014a7970b668430f1b
Belosvet-star <63208646+Belosvet-star@users.noreply.github.com>
2021-03-25 19:32:45 +0200

    Add files via upload

---
+++ willberris/js/main — копия.js
@@ -0,0 +1,184 @@
+const mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+});
+
+
+//cart корзина
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
+
+const openModal = function(){
+	modalCart.classList.add('show');
+}
+const closeModal = function(){
+	modalCart.classList.remove('show');
+}
+
+buttonCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
+
+modalCart.addEventListener('click', function(event) {
+	const target = event.target;
+	if (target.classList.contains('overlay') || //contains - проверка на наличие класса
+		target.classList.contains('modal-close')) {
+		closeModal()
+	}
+});
+
+//плавная прокрутка scrill smooth? по с тандарту быстро проскакивает
+//получим все ссылки у кот класс scrolllink
+
+
+/* (function(){
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+	for (let i = 0; i < scrollLinks.length; i++) {
+		scrollLinks[i].addEventListener('click', function(event){
+			event.preventDefault();
+			const id = scrollLinks[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			})
+			console.log(id);
+		});
+	}
+})() */
+
+//это то же, что и выше, но по другому
+(function(){
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (scrollLink of scrollLinks) {
+		scrollLink.addEventListener('click', function(event){
+			event.preventDefault();
+			const id = scrollLink.getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			})
+			console.log(id);
+
+		});
+	}
+})()
+
+//goods товары
+// const more = document.querySelector('.more');
+const longGoodsList = document.querySelector('.long-goods-list');
+const viewAll = document.querySelectorAll('.view-all');
+const navigationLink = document.querySelectorAll('.navigation-link:not(.view-all)');
+const showAcsessories = document.querySelectorAll('.show-acsessories');
+const showClothing = document.querySelectorAll('.show-clothing');
+
+
+//функци, кот будет получать товары с сервера. У нас этол папка db и обычно это файл json
+
+const getGoods = async function() {
+	const result = await fetch('db/db.json'); //тут может быть урл адрес такого файла
+	//console.log(result);
+	if(!result.ok) {
+		throw 'ошибочка вышла:  ' + result.status
+	}
+	return await result.json();
+};
+
+/* getGoods().then(function(data){
+	console.log(data)
+}); */
+//console.log(getGoods());
+
+//выводить карточки.
+//вначале их создаем
+const createCard = function (objCard) {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6'
+
+	const {label, name, img, description, id, price} = objCard; //деструктуризация
+
+	card.innerHTML = `
+		<div class="goods-card">
+			${label ?
+				'<span class="label">${label}</span>' :
+				''}
+			<img
+				src="db/${img}"
+				alt="${name}"
+				class="goods-image"
+			/>
+			<h3 class="goods-title">${name}</h3>
+			<p class="goods-description">${description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id="${id}">
+				<span class="button-price">$${price}</span>
+			</button>
+		</div>
+	`; //`` где ё это обратные кавычки - это шаблонныу строки, комментарии можно тут убрать
+
+	return card;
+};
+
+//показ этих карточек на странице
+const renderCards = function(data) {
+	longGoodsList.textContent = ''; 
+	const cards = data.map(createCard);
+	longGoodsList.append(...cards);
+	document.body.classList.add('show-goods')
+};
+
+const ahowAll =  function(event){
+	event.preventDefault();
+	getGoods().then(renderCards);
+};
+viewAll.forEach(function(elem){
+	elem.addEventListener('click', function(event){
+		event.preventDefault();
+		getGoods().then(renderCards);
+	});
+
+});
+
+
+//фильтруем карточки по фенскому, мужсткому еще какому-то
+const filterCards = function(field, value){
+	getGoods()
+	.then(function (data) {
+		const filteredGoods = data.filter(function(good) {
+			return good[field] === value
+		});
+		return filteredGoods;
+	})
+	.then(renderCards);
+};
+
+navigationLink.forEach(function (link) {
+	link.addEventListener('click', function(event) {
+		event.preventDefault();
+		//console.log(link);
+		const field = link.dataset.field;
+		const value = link.textContent;
+		//console.log(field, value);
+		filterCards(field, value);
+	})
+});
+
+
+showAcsessories.forEach(item => {
+	item.addEventListener('click', e => {
+		e.preventDefault();
+		filterCards('category', 'Accessories');
+	});
+});
+
+showClothing.forEach(item => {
+	item.addEventListener('click', e => {
+		e.preventDefault();
+		filterCards('category', 'Clothing');
+	});
+});
+
---
+++ willberris/js/main.js
@@ -0,0 +1,295 @@
+const mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+});
+
+
+//cart корзина
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
+const longGoodsList = document.querySelector('.long-goods-list');
+const viewAll = document.querySelectorAll('.view-all');
+const navigationLink = document.querySelectorAll('.navigation-link:not(.view-all)');
+const showAcsessories = document.querySelectorAll('.show-acsessories');
+const showClothing = document.querySelectorAll('.show-clothing');
+
+
+const getGoods = async () => {
+	const result = await fetch('db/db.json'); //тут может быть урл адрес такого файла
+	//console.log(result);
+	if(!result.ok) {
+		throw 'ошибочка вышла:  ' + result.status
+	}
+	return await result.json();
+};
+
+const openModal = () => {
+	modalCart.classList.add('show');
+	cart.renderCart();
+}
+const closeModal = () => {
+	modalCart.classList.remove('show');
+}
+
+buttonCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
+
+modalCart.addEventListener('click', function(event) {
+	const target = event.target;
+	if (target.classList.contains('overlay') || //contains - проверка на наличие класса
+		target.classList.contains('modal-close')) {
+		closeModal()
+	}
+});
+
+//плавная прокрутка scroll smooth, по с тандарту быстро проскакивает
+//получим все ссылки у кот класс scrolllink
+
+
+
+//это то же, что и выше, но по другому
+(function(){
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (scrollLink of scrollLinks) {
+		scrollLink.addEventListener('click', event => {
+			event.preventDefault();
+			const id = scrollLink.getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			})
+			console.log(id);
+
+		});
+	}
+})()
+
+//goods товары
+
+
+
+//функци, кот будет получать товары с сервера. У нас этол папка db и обычно это файл json
+
+
+
+//выводить карточки.
+//вначале их создаем
+const createCard = function (objCard) {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6'
+
+	const {label, name, img, description, id, price} = objCard; //деструктуризация
+
+	card.innerHTML = `
+		<div class="goods-card">
+			${label ?
+				'<span class="label">${label}</span>' :
+				''}
+			<img
+				src="db/${img}"
+				alt="${name}"
+				class="goods-image"
+			/>
+			<h3 class="goods-title">${name}</h3>
+			<p class="goods-description">${description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id="${id}">
+				<span class="button-price">$${price}</span>
+			</button>
+		</div>
+	`; //`` где ё это обратные кавычки - это шаблонныу строки, комментарии можно тут убрать
+
+	return card;
+};
+
+//показ этих карточек на странице
+const renderCards = function(data) {
+	longGoodsList.textContent = ''; 
+	const cards = data.map(createCard);
+	longGoodsList.append(...cards);
+	document.body.classList.add('show-goods')
+};
+
+const showAll =  event => {
+	event.preventDefault();
+	getGoods().then(renderCards);
+};
+viewAll.forEach(function(elem){
+	elem.addEventListener('click', function(event){
+		event.preventDefault();
+		getGoods().then(renderCards);
+	});
+
+});
+
+
+//фильтруем карточки по фенскому, мужсткому еще какому-то
+const filterCards = function(field, value){
+	getGoods()
+		.then(data => data.filter(good => good[field] === value	))
+		.then(renderCards);
+};
+
+navigationLink.forEach(function (link) {
+	link.addEventListener('click', event => {
+		event.preventDefault();
+		//console.log(link);
+		const field = link.dataset.field;
+		const value = link.textContent;
+		//console.log(field, value);
+		filterCards(field, value);
+	})
+});
+
+
+showAcsessories.forEach(item => {
+	item.addEventListener('click', e => {
+		e.preventDefault();
+		filterCards('category', 'Accessories');
+	});
+});
+
+showClothing.forEach(item => {
+	item.addEventListener('click', e => {
+		e.preventDefault();
+		filterCards('category', 'Clothing');
+	});
+});
+
+// корзина в всплывающем окне по методике es6
+const cartTableGoods = document.querySelector('.cart-table__goods');
+const cardTableTotal = document.querySelector('.card-table__total');
+
+const cart = {
+	//cartGoods: [],
+	
+	cartGoods: [],
+	/* cartGoods: [
+		{
+			id: "099",
+			name: "часы",
+			price: 1000,
+			count: 1,
+		},
+		{
+			id: "0100",
+			name: "кеды какие-то",
+			price: 10,
+			count: 5,
+		},
+	], */
+	renderCart(){
+		cartTableGoods.textContent = '';
+		this.cartGoods.forEach(({id, name, price, count}) => {
+			const trGood = document.createElement('tr');
+			trGood.className = 'cart-item';
+			trGood.dataset.id = id;
+// это один вариант удаления с data-id
+			/* trGood.innerHTML = `
+				<td>${name}</td>
+				<td>${price}$</td>
+				<td><button class="cart-btn-minus" data-id="${id}">-</button></td>
+				<td>${count}</td>
+				<td><button class="cart-btn-plus" data-id="${id}">+</button></td>
+				<td>${price * count}$</td>
+				<td><button class="cart-btn-delete" data-id="${id}">x</button></td>
+			`;
+			cartTableGoods.append(trGood); */
+			trGood.innerHTML = `
+				<td>${name}</td>
+				<td>${price}$</td>
+				<td><button class="cart-btn-minus" >-</button></td>
+				<td>${count}</td>
+				<td><button class="cart-btn-plus" >+</button></td>
+				<td>${price * count}$</td>
+				<td><button class="cart-btn-delete" >x</button></td>
+			`;
+			cartTableGoods.append(trGood);
+		});
+
+		const totalPrice = this.cartGoods.reduce((sum, item) => {
+			return sum + (item.price * item.count);
+		}, 0);
+		cardTableTotal.textContent = totalPrice + '$'
+
+	},
+	deleteGood(id) {
+		this.cartGoods = this.cartGoods.filter(item => id !== item.id);
+		this.renderCart();
+	},// эта строчка вместо foo(param){}
+	minusGood(id) {
+		for (const item of this.cartGoods) {
+			if (item.id === id) {
+				if (item.count <= 1) {
+					this.deleteGood(id)
+				} else {
+					item.count--;
+				}
+				break;
+			}
+		}
+		this.renderCart();
+	},
+	plusGood(id) {
+		for(const item of this.cartGoods) {
+			if (item.id === id) {
+				item.count++;
+				break;
+			}
+		}
+		this.renderCart();
+	},
+	addCartGoogs(id){
+		const goodItem = this.cartGoods.find(item => item.id === id);
+		if (goodItem) {
+			this.plusGood(id);
+		} else {
+			getGoods()
+			.then(data => data.find(item => item.id === id))
+			.then(({id, name, price }) => {
+				this.cartGoods.push({
+					id,
+					name,
+					price,
+					count: 1
+				});
+			});
+		}
+	},
+}
+//cart.addCartGoogs('021');
+document.body.addEventListener('click', event => {
+	const addToCart = event.target.closest('.add-to-cart');
+	if (addToCart) {
+		cart.addCartGoogs(addToCart.dataset.id)
+	}
+})
+
+cartTableGoods.addEventListener('click', event => {
+	const target = event.target;
+
+	if(target.tagName === "BUTTON") {
+		const id = target.closest('.cart-item').dataset.id;
+
+		if (target.classList.contains('cart-btn-delete')) {
+		//cart.deleteGood(target.dataset.id); // это один вариант удаления с data-id
+		
+			cart.deleteGood(id);
+		};
+		if (target.classList.contains('cart-btn-minus')) {
+			
+			cart.minusGood(id);
+		};
+		if (target.classList.contains('cart-btn-plus')) {
+			
+			cart.plusGood(id);
+		};
+	}
+	
+})
