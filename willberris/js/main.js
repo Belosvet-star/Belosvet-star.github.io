8e1a938b573d28db495174b132a3ad6b9af40412
Belosvet-star <63208646+Belosvet-star@users.noreply.github.com>
2021-03-26 21:06:57 +0200

    Add files via upload

--- willberris/js/main.js
+++ willberris/js/main.js
@@ -18,6 +18,7 @@ const viewAll = document.querySelectorAll('.view-all');
 const navigationLink = document.querySelectorAll('.navigation-link:not(.view-all)');
 const showAcsessories = document.querySelectorAll('.show-acsessories');
 const showClothing = document.querySelectorAll('.show-clothing');
+const cartCount = document.querySelector('.cart-count');
 
 const checkGoods = () => {
 
@@ -170,7 +171,7 @@ showClothing.forEach(item => {
 // корзина в всплывающем окне по методике es6
 const cartTableGoods = document.querySelector('.cart-table__goods');
 const cardTableTotal = document.querySelector('.card-table__total');
-const cartCount = document.querySelector('.cart-count');
+
 const btnDanger = document.querySelector('.btn-danger');
 
 const cart = {
@@ -192,11 +193,13 @@ const cart = {
 		},
 	], */
 	countQuantity() {
-		cartCount.textContent = this.cartGoods.reduce((sum, item) => {
+		const count = this.cartGoods.reduce((sum, item) => {
 			return sum + item.count
 		}, 0)
+		cartCount.textContent = count ? count : '';
 	},
 	clearCart() {
+		console.log(this);
 		this.cartGoods.length = 0;
 		this.countQuantity();
 		this.renderCart();
@@ -238,8 +241,8 @@ const cart = {
 	},
 	deleteGood(id) {
 		this.cartGoods = this.cartGoods.filter(item => id !== item.id);
-		this.renderCart();
 		this.countQuantity()
+		this.renderCart();
 	},// эта строчка вместо foo(param){}
 	minusGood(id) {
 		for (const item of this.cartGoods) {
@@ -252,8 +255,8 @@ const cart = {
 				break;
 			}
 		}
-		this.renderCart();
 		this.countQuantity()
+		this.renderCart();
 	},
 	plusGood(id) {
 		for(const item of this.cartGoods) {
@@ -262,8 +265,8 @@ const cart = {
 				break;
 			}
 		}
-		this.renderCart();
 		this.countQuantity()
+		this.renderCart();
 	},
 	addCartGoogs(id){
 		const goodItem = this.cartGoods.find(item => item.id === id);
@@ -318,7 +321,59 @@ cartTableGoods.addEventListener('click', event => {
 		};
 	}
 	
-})
+});
+
+// 4 день
+const modalForm = document.querySelector('.modal-form');
+
+const postData = dataUser => fetch('./server.php', {
+	method: 'POST',
+	body: dataUser,
+});
+
+const validForm = (formData) => {
+	let valid = false;
+
+	for (const [value, name] of formData) {
+		if (value.trim()) {
+			valid = true;
+		}else {
+			valid = false;
+			break;
+		}
+	}
+	return valid;
+}
+
+modalForm.addEventListener('submit', event => {
+	event.preventDefault();
+
+	const formData = new FormData(modalForm);
+
+	
+
+	formData.append('cart', JSON.stringify(cart.cartGoods))
+
+	postData(formData)
+	.then(response => {
+		if (!response.ok) {
+			throw new Error(response.status);
+		}
+		alert('Ваш заказ успешно отправлен, с вами свяжутся в ближайшее время');
+		console.log(response.statusText);
+	})
+	.catch(err => {
+		alert('К сожалению произошла ошибка, повторите позже');
+		console.error(err);
+	})
+	.finally(() => {
+		closeModal();
+		modalForm.reset();
+		cart.clearCart();
+	});
+});
+
+
 
 
 
