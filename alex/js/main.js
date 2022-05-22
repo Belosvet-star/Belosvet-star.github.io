1c5e2186e5038e80f63833076c1903ebf480d0ef
Belosvet-star <63208646+Belosvet-star@users.noreply.github.com>
2022-05-23 01:15:31 +0300

    Add files via upload

--- alex/js/main.js
+++ alex/js/main.js
@@ -2,16 +2,43 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
+/* import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper(); */
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+if (document.body.clientWidth < 619) {
+  swiperService.destroy();
+}
 
 // библиотека aos
-
-
-
-
 AOS.init({
-  disable: 'mobile'
+  /* disable: function () {
+    var maxWidth = 800;
+    return window.innerWidth < maxWidth;
+  } */
+  disable: 'phone'
 });
+
 
 jQuery(document).ready(function ($) {
   $('.popup-content').magnificPopup({
@@ -52,7 +79,71 @@ let isMobile = {
   }
 };
 
-// простое меню
+//---------------------------------------------------------------------------------
+/////////////простое меню
+$(document).ready(function () {
+  //бургер-меню
+  $('.nav-burger').click(function (event) {
+    $('.nav-burger, .main-menu').toggleClass('is-open');
+    $('body').toggleClass('lock');
+  });
+
+  //подсветка активного пункта меню при скролле - должен быть jquery
+  var sections = $('.section'),
+    links = $('nav ul li a'),
+    lis = $('nav ul > li');
+
+  $(window).scroll(function () {
+    var currentPosition = $(this).scrollTop();
+    links.removeClass('current');
+    lis.removeClass('current');
+
+    sections.each(function () {
+      var top = $(this).offset().top,
+        bottom = top + $(this).height();
+
+      if (currentPosition >= top && currentPosition <= bottom) {
+        var link = $('a[href="#' + this.id + '"]');
+        link.addClass('shine');
+        link.parent().addClass('current');
+      } else {
+        link = $('a[href="#' + this.id + '"]');
+        link.removeClass('shine');
+        link.parent().removeClass('current');
+      }
+
+    });
+
+  });
+  //прячется при нажатии на пункт мобильного меню
+  $('nav ul li a').on('click', function () {
+    $('.nav-burger, .main-menu').toggleClass('is-open');
+  });
+});
+
+
+//---------------------------------------------------------------------------------------------------------------
+////// выпадающее меню
+let body = document.querySelector('body');
+if (isMobile.any()) { //убрать эту строчку, если стрелки надо и на десктопе
+  body.classList.add('touch');
+  let arrow = document.querySelectorAll('.arrow');
+  for (i = 0; i < arrow.length; i++) {
+    let thisLink = arrow[i].previousElementSibling;
+    let subMenu = arrow[i].nextElementSibling;
+    let thisArrow = arrow[i];
+
+    thisLink.classList.add('parent');
+    arrow[i].addEventListener('click', function () {
+      console.log('click');
+      subMenu.classList.toggle('open');
+      thisArrow.classList.toggle('active');
+    });
+  }
+} else {//убрать эту строчку, если стрелки надо и на десктопе
+  body.classList.add('mouse');//убрать эту строчку, если стрелки надо и на десктопе
+}//убрать эту строчку, если стрелки надо и на десктопе
+
 $(document).ready(function () {
   //бургер-меню
   $('.nav-burger').click(function (event) {
@@ -89,10 +180,7 @@ $(document).ready(function () {
   $('nav ul li a').on('click', function () {
     $('.nav-burger, .main-menu').toggleClass('is-open');
   });
-
 });
-
-
 
 //прокрутка кнопка вверх jquery
 jQuery(($) => {
@@ -108,21 +196,7 @@ jQuery(($) => {
 
 //magnific-popup
 
-$(document).on('click', '.image-link', function () {
-  $('.image-link').magnificPopup({
-    
-    type: 'image',
 
-    closeBtnInside: true,
-   /*  mainClass: 'mfp-with-zoom mfp-img-mobile',
-     zoom: {
-      enabled: true,
-      duration: 300,
-      opener: function (element) {
-        return element.find('img');
-      }
-    }  */
-   
 
-  })
-})
+
+
